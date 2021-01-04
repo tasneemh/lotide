c02fa57560c80a66fe6e178e14ad16ea3e67259a
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-04 20:08:39 +0000

    Assert function created

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌ Assertion failed: " + actual + " !== " + expected);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, "2");
+assertEqual("3", "3");
+assertEqual(1, 4);
