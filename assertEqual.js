e58c6b474b776e888d56304faf2fe4960e68f2b2
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-04 20:18:45 +0000

    Template strings used in the code

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.log("❌❌❌ Assertion failed: " + actual + " !== " + expected);
+    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
   }
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
