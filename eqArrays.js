ce56f75704bdea49481227aaa078fa1c32e6928a
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-05 16:53:54 +0000

    Change of code lines

--- eqArrays.js
+++ eqArrays.js
@@ -10,11 +10,14 @@ const eqArrays = function (arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] === arr2[i]) {
       count++;
+      //return true;
     } else {
       count = count;
+      //return false;
     }
   }
-  if (count === 3) {
+
+  if (count === arr1.length) {
     return true;
   } else {
     return false;
@@ -23,3 +26,4 @@ const eqArrays = function (arr1, arr2) {
 console.log(eqArrays([1, 2, 3], [1, 2, "3"]));
 console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
