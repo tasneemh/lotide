2eeb59748161ccb6c2fa6abc244fa17c1aa0733f
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-07 20:55:18 +0000

    code added

---
+++ takeUntil.js
@@ -0,0 +1,29 @@
+const takeUntil = function (array, callback) {
+  const results = [];
+  //console.log(array);
+  for (let item of array) {
+    if (!callback(item)) {
+      results.push(item);
+    } else {
+      return results;
+    }
+  }
+  return results;
+};
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1);
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+console.log(results2);
