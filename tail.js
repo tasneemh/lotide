a7922a6f1d6f609777b2d5344ca5fc7f266f8edb
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-10 21:01:47 +0000

    refactored tail.js

--- tail.js
+++ tail.js
@@ -1,14 +1,6 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
-  }
-};
 const tail = function (arr) {
   return arr.slice(1);
 };
 const result = tail([1, 2, 3]);
-assertEqual(result.length, 2);
-assertEqual(result[0], 2);
-assertEqual(result[1], 3);
+
+module.exports = result;
