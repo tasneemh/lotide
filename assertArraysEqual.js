896a5a1c6ece29ab45b60dd68a7a0c3191d28643
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-10 21:39:05 +0000

    refactored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
-const eqArrays = function (arr1, arr2) {
-  count = 0;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      count++;
-    }
-  }
-  if (count === arr1.length) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
+const eqArrays = require("./eqArrays");
 const assertArraysEqual = function (actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
@@ -19,8 +6,5 @@ const assertArraysEqual = function (actual, expected) {
     console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
   }
 };
-/*
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, "3"]);
-*/
+
 module.exports = assertArraysEqual;
