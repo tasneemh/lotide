0e980489f058da437bc3f32efe2485a69f58c1e4
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-10 23:12:22 +0000

    add mocha and chai

---
+++ test/tailTest.js
@@ -0,0 +1,13 @@
+const assert = require("chai").assert;
+const tail = require("../tail");
+describe("#", () => {
+  it("returns [2,3] for [1,2,3]", () => {});
+  assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+});
+/*
+const assertEqual = require("../assertEqual");
+const result = require("../tail");
+assertEqual(result.length, 2);
+assertEqual(result[0], 2);
+assertEqual(result[1], 3);
+*/
