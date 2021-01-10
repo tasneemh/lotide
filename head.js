13d443762896cbfc53df081f0cde7173a188d654
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-10 20:51:03 +0000

    refatored head.js

--- head.js
+++ head.js
@@ -1,15 +1,7 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
-  }
-};
 const head = function (arr) {
   //returns first element of an array
   //we can use arr[0] as well
   return arr.shift();
 };
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), [1]);
+
+module.exports = head;
