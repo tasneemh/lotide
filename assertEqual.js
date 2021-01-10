717f496465ab5929175465bfc21824a08b30bc35
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-10 20:52:24 +0000

    refactored assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -5,8 +5,4 @@ const assertEqual = function (actual, expected) {
     console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, "2");
-assertEqual("3", "3");
-assertEqual(1, 4);
+module.exports = assertEqual;
