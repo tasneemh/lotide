f4dac62fd9c786e62cb3df75544dc500f6b586c8
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-01-11 00:34:10 +0000

    add index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const head = require("./head");
+const middle = require("./middle");
+const tail = require("./tail");
+const eqArrays = require("./eqArrays");
+const assertEqual = require("./assertEqual");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const flatten = require("./flatten");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+};
